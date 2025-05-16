2b4547a0781191357c0fa95f9c8577b7b88cee2b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-16 18:05:48 +0800

    mi7150-mainline: Make use of /odm
    Change-Id: I994710e5c0688c406ee7905cd60c5f30d4f448a7

--- davinci_mainline/device.mk
+++ davinci_mainline/device.mk
@@ -22,7 +22,7 @@ TARGET_SCREEN_HEIGHT := 2340
 
 # Firmware
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,external/firmware-xiaomi-davinci/lib/firmware/,$(TARGET_COPY_OUT_VENDOR)/firmware/)
+    $(call find-copy-subdir-files,*,external/firmware-xiaomi-davinci/lib/firmware/,$(TARGET_COPY_OUT_ODM)/firmware/)
 
 # Init
 PRODUCT_COPY_FILES += \
