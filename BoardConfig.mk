2ba4042cb1394856eb140aec8055565a816f4f07
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-16 18:37:21 +0800

    mi7150-mainline: Bringup battery fuelgauge
    Change-Id: I79b6170b184f7096ef5b393b15ccecb9d6ec9c2d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,9 +59,9 @@ TARGET_DTB_LIST_WILDCARD := qcom/sm7150-xiaomi-sweet
 endif
 
 # Kernel modules
-#BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/configs/modules.load))
-#BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/configs/modules.load))
-#RECOVERY_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/configs/modules.include))
+BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modprobe/modules.load.recovery))
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modprobe/modules.load))
+RECOVERY_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/modprobe/modules.include.recovery))
 
 # Partitions
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
