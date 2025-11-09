aaa9dcf5414ac4d6c6b282cc9352566827423270
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-10 02:28:38 +0800

    mi7150-mainline: Let user flash the appropriate U-Boot build first
    * U-Boot builds has multiple variants for a single particular device.
    
    Change-Id: I9c3900aaee05c1d0e730db549f3261043d9e3793

--- davinci_mainline/misc/releasetools.py
+++ davinci_mainline/misc/releasetools.py
@@ -36,5 +36,4 @@ def AddImage(info, basename, dest, images_dir = "IMAGES"):
 
 def OTA_InstallEnd(info):
   AddImage(info, "cust.img", "/dev/block/by-name/cust", images_dir = "RADIO")
-  AddImage(info, "u-boot-sm7150-xiaomi-davinci-usb-peripheral.img", "/dev/block/by-name/boot", images_dir = "RADIO")
   return
